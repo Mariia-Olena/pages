ee0f28fe8a083bf60f1a107dff0d40a1b66212ba
Mariia-Olena <mariia.stus@gmail.com>
2023-02-08 15:04:34 +0200

    created pages app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  pages = [
+    {
+      title: 'Chernivtsi',
+      image: 'assets/images/chernivtsi.jpg',
+    },
+    {
+      title: 'Kyiv',
+      image: 'assets/images/kyiv.jpg',
+    },
+    {
+      title: 'Summer Carpathian Mountains',
+      image: 'assets/images/summer_carpathian_mountains.jpg',
+    },
+    {
+      title: 'Winter Carpathian Mountains',
+      image: 'assets/images/winter_carpathian_mountains.jpg',
+    },
+    {
+      title: 'Odesa',
+      image: 'assets/images/odesa.jpg',
+    },
+    {
+      title: 'Lviv',
+      image: 'assets/images/lviv.jpg',
+    },
+  ];
+
+  currentIndex = 0;
 }
